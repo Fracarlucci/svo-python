c0efff3736a9846b572d729d3f70fe1a5a243728
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2023-04-17 21:05:55 +0200

    Minor fixes

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from fastapi import FastAPI
 import uvicorn
-from config import app, db
+from config import app
 from routers import northbound, southbound, SVO, vu_interface
-
-#@app.route('/', methods=['GET', 'POST'])
-#def db():
-    #db.create_all()
-    #db.session.commit()
-    #return"ok"
-#flask run -h localhost -p 3000
 
 app.include_router(southbound.router)
 app.include_router(northbound.router)
@@ -17,4 +9,3 @@ app.include_router(vu_interface.router)
 
 if __name__ == '__main__':
     uvicorn.run(app)
-
--- config.py
+++ config.py
@@ -3,10 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#parametri per SOR
-min_interval = float(5) ##5 minuti
-min_n_meetings = 3
-
 app = FastAPI()
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./svo.db"
@@ -15,24 +11,12 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
 
 Base = declarative_base()
 Base.metadata.create_all(engine)
 
+# TODO add cache
+
 # cache = Cache(config={'CACHE_TYPE': 'simple'})
 # cache.init_app(app)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
